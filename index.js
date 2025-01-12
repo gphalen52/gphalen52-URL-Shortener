de85873d1f6bfd7b216a9dd2ea4bbaa50f2da614
George Phalen <gphalen52@gmail.com>
2025-01-12 22:22:24 +0000

    Setup

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,6 +17,23 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+
+function dataManagement(action, input) {
+  const filePath = path.join(__dirname, 'public', 'data.json');
+
+  // Check if the file exists, create it if not
+  if (!fs.existsSync(filePath)) {
+    try {
+      // Create an empty file if it does not exist
+      fs.writeFileSync(filePath, JSON.stringify({}), 'utf8');
+      console.log('File created successfully.');
+    } catch (err) {
+      console.error('Error creating the file:', err);
+    }
+  }
+}
+
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
